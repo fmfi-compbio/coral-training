f91ab5a56093de33850029db4bd386ed38f20716
Peter Peresini <ppershing@gmail.com>
2020-09-24 21:03:14 +0200

    Update training script with more arguments

--- train.py
+++ train.py
@@ -16,23 +16,37 @@ def main():
     parser.add_argument("--config", required=True)
     parser.add_argument("--schedule", default="full")
     parser.add_argument("--data_dir", default="/scratch/basecall_train")
+    parser.add_argument("--batch_size", default=15, type=int)
     parser.add_argument("--outname")
+    parser.add_argument("--optimizer", default="adam")
     args = parser.parse_args()
-    
+
     # Prepare model
     schedule = schedules.schedules[args.schedule]
     net_cfg = cfg.configs[args.config]()
 
     model = net.make_model(net_cfg)
-    #model.compile(optimizer=tf.keras.optimizers.Adam(), loss=net.CTCLoss())
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=net.CTCLoss())
+    def get_optimizer(variant):
+        if variant=="adam":
+            return tf.keras.optimizers.Adam(beta_1=0.9)
+        if variant=="novograd":
+            import tensorflow_addons as tfa
+            return tfa.optimizers.NovoGrad(beta_1=0.9)
+
+    model.compile(optimizer=get_optimizer(args.optimizer), loss=net.CTCLoss())
+    print(model.summary())
 
     # load files
     X, Y = data_loader.load_dir(args.data_dir)
 
     rng = random.Random(4747)
-    def get_batch(*, batch_size=100, leng=5000):
+    SEQLEN=5000
+    SEQLEN=5004
+    #SEQLEN=600
+    #SEQLEN=3000
+
+    def get_batch(*, batch_size=100, leng=SEQLEN):
         x, y = data_loader.prep_batch(X, Y, rng, batch_size, leng)
         res = (
             tf.constant(x),
@@ -40,12 +54,13 @@ def main():
         )
         return res
 
-    x_test, y_test = get_batch(batch_size=100, leng=5000)
+    x_test, y_test = get_batch(batch_size=100, leng=SEQLEN)
     print(x_test[0])
+
     epoch = 1
 
     def _save(model, fname):
-        quant = converter.convert(model, get_batch)
+        quant = converter.convert(model, get_batch, STEPS=SEQLEN, BATCH_SIZE=4)
         with open(fname, "wb") as f:
             f.write(quant)
         print(fname, len(quant)) 
@@ -55,16 +70,25 @@ def main():
     for (n_epochs, lr) in schedule:
         tf.keras.backend.set_value(model.optimizer.lr, lr)
         for _ in range(n_epochs):
-            B_SIZE = 15
+            B_SIZE = args.batch_size
             B_CNT = 1500 // B_SIZE
-            model.fit(get_batch(batch_size=B_SIZE, leng=5000) for _ in range(B_CNT))
-            
+            model.fit(get_batch(batch_size=B_SIZE, leng=SEQLEN) for _ in range(B_CNT))
             stats = align_stats.align_dist(model(x_test), y_test)
             print("epoch ", epoch, stats[0]/stats[1])
-            epoch += 1
             print(epoch, stats, file=f_stat)
             if epoch % 100 == 0:
                 _save(model, f"models/{args.outname or args.config}.{epoch}")
+            
+            epoch += 1
+
+    if True:
+        print("folding") 
+        for l in model._layers[2]._layers[0]:
+            try:
+                l.fold_bn()
+            except:
+                pass
+        model.compile(optimizer=tf.keras.optimizers.Adam(), loss=net.CTCLoss())
 
     _save(model, f"models/{args.outname or args.config}.tflite")
 
